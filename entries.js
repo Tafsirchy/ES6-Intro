efcf0be0c53b1106dd2a85b8174bb58941facbbe
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-04 09:22:18 +0600

    entries

---
+++ entries.js
@@ -0,0 +1,15 @@
+const employee = {
+    name: 'John',
+    designation: 'developer',
+    salary: '50000',
+    experience: 1
+}
+
+const keys = Object.keys(employee)
+console.log(keys)
+
+const values = Object.values(employee)
+console.log(values)
+
+const entries = Object.entries(employee)
+console.log(entries)
