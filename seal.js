3899fa27d2c756e5f140772470189aea8ccb993f
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-04 09:33:42 +0600

    seal

---
+++ seal.js
@@ -0,0 +1,19 @@
+
+
+
+const employee = {
+    name: 'John',
+    age: 30,
+    designation: 'developer',
+    salary: 50000,
+    experience: 1
+
+}
+Object.seal(employee);
+
+delete employee.experience;
+// delete employee.salary;
+
+employee.location = 'Dhaka'
+employee.salary = employee.salary + 6000
+console.log(employee);
